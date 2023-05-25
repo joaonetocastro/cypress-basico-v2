ca19766632eb78bf88d881ecb0b191c351f14d2b
João Neto C <j.neto111@gmail.com>
2023-05-25 06:33:15 -0300

    feat: valida error

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -10,7 +10,7 @@ describe('Central de Atendimento ao Cliente TAT', function () {
         cy.get('input[name="firstName"]').should('be.visible').type('Olá mundo!').should('have.value', 'Olá mundo!')
     })
 
-    it.only('preencha os campos obrigatórios e envia o formulario', function() {
+    it('preencha os campos obrigatórios e envia o formulario', function() {
         const longText = 'lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum '
         
         cy.get('#firstName').type('João')
@@ -19,5 +19,14 @@ describe('Central de Atendimento ao Cliente TAT', function () {
         cy.get('#open-text-area').type(longText, {delay: 0})
         cy.get('button[type=submit]').click()
         cy.get('.success').should('be.visible')
+    })
+
+    it('exibe mensagem de erro ao submeter o formulário com um email com formatação errada', function () {
+        cy.get('#firstName').type('João')
+        cy.get('#lastName').type('Neto C.')
+        cy.get('#email').type('email_invalido')
+        cy.get('#open-text-area').type('teste')
+        cy.get('button[type=submit]').click()
+        cy.get('.error').should('be.visible')
     })
 })
