1450ea2321b0c51425e00b5d7331661cb733c721
João Neto C <j.neto111@gmail.com>
2023-05-25 06:07:52 -0300

    feat: execicio extra 1

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -1,8 +1,23 @@
 /// <reference types="Cypress" />
 
 describe('Central de Atendimento ao Cliente TAT', function () {
-    it('verifica o título da aplicação', function () {
+    beforeEach(() => {
         cy.visit('./src/index.html')
+    })
+
+    it('verifica o título da aplicação', function () {
         cy.title().should('be.equal', 'Central de Atendimento ao Cliente TAT')
+        cy.get('input[name="firstName"]').should('be.visible').type('Olá mundo!').should('have.value', 'Olá mundo!')
+    })
+
+    it.only('preencha os campos obrigatórios e envia o formulario', function() {
+        const longText = 'lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum '
+        
+        cy.get('#firstName').type('João')
+        cy.get('#lastName').type('Neto C.')
+        cy.get('#email').type('joaonetoc@gmail.com')
+        cy.get('#open-text-area').type(longText, {delay: 0})
+        cy.get('button[type=submit]').click()
+        cy.get('.success').should('be.visible')
     })
 })
